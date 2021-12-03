f57177fde048f9edffa78b24dd5fb1e8272b94b4
zavet-code <yauheni.zavatski13@gmail.com>
2021-12-03 17:33:16 +0300

    add input field to add csv file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import eel
 import csv
 import io
 import re
 from datetime import datetime
+
+eel.init('web')
+
+eel.start('main.html', size=(700, 700))
 
 main_csv = 'eggs1.csv'
 sorted_csv = 'example.csv'
@@ -14,27 +19,24 @@ def ticket_number_key(row):
     result = re.findall('\d+', row[1])
     return result
 
+def letter_key(row):
+    result = re.findall('\w+', row[3])
+    return result
+
 def csv_writer(text, file_path, nline):
     s = io.StringIO(text)
     with open(file_path, 'a', newline='') as f:
         for line in s:
             f.write(line + nline)
-            
 
-with open(main_csv, newline='') as csvfile:
-    first_line = csvfile.readline()
-    spamreader = list(csv.reader(csvfile, delimiter='|'))
-    spamreader.sort(key=ticket_number_key)
-    csv_writer(first_line, sorted_csv, '')
-    for row in spamreader:
-        csv_writer('| '.join(row), sorted_csv, '\n')
-        print(datetime.strptime(row[9].strip(), "%d/%b/%y %H:%M %p"))
-        print('| '.join(row))
-
- 
-        
-
-
-
-
-
+@eel.expose
+def get_sort_csv(main_csv, delim , rev):           #delim = '|' rev = True or False
+    with open(main_csv, newline='') as csvfile:
+        first_line = csvfile.readline()
+        spamreader = list(csv.reader(csvfile, delimiter = delim))
+        spamreader.sort(key = letter_key, reverse = False)
+        csv_writer(first_line, sorted_csv, '')
+        for row in spamreader:
+            csv_writer('| '.join(row), sorted_csv, '\n')
+            print(datetime.strptime(row[9].strip(), "%d/%b/%y %H:%M %p"))
+            print('| '.join(row))
